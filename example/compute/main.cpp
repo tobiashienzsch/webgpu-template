643dddd9861cd6cff82254dc235994d37bd7c4c0
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-05-19 02:19:36 +0200

    Improve compute example

--- example/compute/main.cpp
+++ example/compute/main.cpp
@@ -25,18 +25,17 @@ auto main(int, char**) -> int {
     }
 
     device.SetUncapturedErrorCallback(tobi::gpu::errorCallback, nullptr);
-    // tobi::gpu::inspectDevice(device);
 
     wgpu::Queue queue = device.GetQueue();
 
     // Create buffers
-    const size_t dataSize = 1024 * sizeof(float);
+    const size_t dataSizeInBytes = 1024 * sizeof(float);
     auto dataLHS = std::vector<float>(1024, 1.0f);
     auto dataRHS = std::vector<float>(1024, 2.0f);
     auto dataOut = std::vector<float>(1024, 0.0f);
 
     auto buffer = wgpu::BufferDescriptor{};
-    buffer.size = dataSize;
+    buffer.size = dataSizeInBytes;
     buffer.usage =
         wgpu::BufferUsage::Storage | wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::CopySrc;
 
@@ -44,8 +43,8 @@ auto main(int, char**) -> int {
     auto rhs = device.CreateBuffer(&buffer);
     auto out = device.CreateBuffer(&buffer);
 
-    queue.WriteBuffer(lhs, 0, dataLHS.data(), dataSize);
-    queue.WriteBuffer(rhs, 0, dataRHS.data(), dataSize);
+    queue.WriteBuffer(lhs, 0, dataLHS.data(), dataSizeInBytes);
+    queue.WriteBuffer(rhs, 0, dataRHS.data(), dataSizeInBytes);
 
     auto shaderModule = tobi::gpu::createShaderModule(device, ShaderCode);
 
@@ -55,33 +54,6 @@ auto main(int, char**) -> int {
     pipelineDesc.compute.entryPoint = "main";
     wgpu::ComputePipeline pipeline = device.CreateComputePipeline(&pipelineDesc);
 
-    // wgpu::BindGroupLayoutEntry entries[3];
-    // entries[0].binding = 0;
-    // entries[0].visibility = wgpu::ShaderStage::Compute;
-    // entries[0].buffer.type = wgpu::BufferBindingType::ReadOnlyStorage;
-    // entries[1].binding = 1;
-    // entries[1].visibility = wgpu::ShaderStage::Compute;
-    // entries[1].buffer.type = wgpu::BufferBindingType::ReadOnlyStorage;
-    // entries[2].binding = 2;
-    // entries[2].visibility = wgpu::ShaderStage::Compute;
-    // entries[2].buffer.type = wgpu::BufferBindingType::Storage;
-
-    // wgpu::BindGroupLayoutDescriptor bdlDescriptor;
-    // bdlDescriptor.entryCount = std::size(entries);
-    // bdlDescriptor.entries = std::data(entries);
-    // wgpu::BindGroupLayout bindGroupLayout = device.CreateBindGroupLayout(&bdlDescriptor);
-
-    // wgpu::PipelineLayoutDescriptor pipelineDescriptor;
-    // pipelineDescriptor.bindGroupLayoutCount = 1;
-    // pipelineDescriptor.bindGroupLayouts = std::addressof(bindGroupLayout);
-    // wgpu::PipelineLayout pl = device.CreatePipelineLayout(&pipelineDescriptor);
-
-    // wgpu::ComputePipelineDescriptor csDesc;
-    // // csDesc.layout = pl;
-    // csDesc.compute.module = shaderModule;
-    // csDesc.compute.entryPoint = "main";
-    // auto computePipeline = device.CreateComputePipeline(&csDesc);
-
     wgpu::BindGroupLayout bindGroupLayout = pipeline.GetBindGroupLayout(0);
 
     // Create the bind group
@@ -89,17 +61,17 @@ auto main(int, char**) -> int {
     bgEntries[0].binding = 0;
     bgEntries[0].buffer = lhs;
     bgEntries[0].offset = 0;
-    bgEntries[0].size = dataSize;
+    bgEntries[0].size = dataSizeInBytes;
 
     bgEntries[1].binding = 1;
     bgEntries[1].buffer = rhs;
     bgEntries[1].offset = 0;
-    bgEntries[1].size = dataSize;
+    bgEntries[1].size = dataSizeInBytes;
 
     bgEntries[2].binding = 2;
     bgEntries[2].buffer = out;
     bgEntries[2].offset = 0;
-    bgEntries[2].size = dataSize;
+    bgEntries[2].size = dataSizeInBytes;
 
     auto bindGroupDesc = wgpu::BindGroupDescriptor{};
     bindGroupDesc.layout = bindGroupLayout;
@@ -120,29 +92,36 @@ auto main(int, char**) -> int {
 
     // Read the dataOut
     wgpu::BufferDescriptor readbackDesc;
-    readbackDesc.size = dataSize;
+    readbackDesc.size = dataSizeInBytes;
     readbackDesc.usage = wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::MapRead;
     wgpu::Buffer readbackBuffer = device.CreateBuffer(&readbackDesc);
 
     encoder = device.CreateCommandEncoder();
-    encoder.CopyBufferToBuffer(out, 0, readbackBuffer, 0, dataSize);
+    encoder.CopyBufferToBuffer(out, 0, readbackBuffer, 0, dataSizeInBytes);
     commands = encoder.Finish();
     queue.Submit(1, &commands);
 
+    auto done = false;
     readbackBuffer.MapAsync(
-        wgpu::MapMode::Read, 0, dataSize,
+        wgpu::MapMode::Read, 0, dataSizeInBytes,
         [](WGPUBufferMapAsyncStatus status, void* userdata) {
             assert(status == WGPUBufferMapAsyncStatus_Success);
+            *static_cast<bool*>(userdata) = true;
         },
-        nullptr);
+        static_cast<void*>(&done));
 
+    while (not done) {
 #if not defined(__EMSCRIPTEN__)
-    // Tick needs to be called in Dawn to display validation errors
-    device.Tick();
+        // Tick needs to be called in Dawn to display validation errors
+        device.Tick();
+        instance.ProcessEvents();
+#else
+        emscripten_sleep(2);
 #endif
+    }
 
-    // const void* mappedData = readbackBuffer.GetMappedRange();
-    // std::memcpy(dataOut.data(), mappedData, dataSize);
+    const void* mappedData = readbackBuffer.GetConstMappedRange();
+    std::memcpy(dataOut.data(), mappedData, dataSizeInBytes);
     readbackBuffer.Unmap();
 
     // Print dataOut
